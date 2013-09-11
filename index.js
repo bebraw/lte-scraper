bd4755641430b1f023ac05707a7def9d584c9004
Juho Vepsalainen <bebraw@gmail.com>
2013-09-11 12:40:52 +0300

    Parse lte networks based on wikipedia data

--- index.js
+++ index.js
@@ -8,15 +8,53 @@ var cheerio = require('cheerio');
 main();
 
 function main() {
-    fs.readFile('lte.html', {
+    /*fs.readFile('lte.html', {
         encoding: 'utf8'
     }, function(err, data) {
         if(err) return console.error(err);
 
         var d = scrapeLte(data);
 
-        console.log(prettyJSON(d));
+        prettyJSON(d);
+    });*/
+    fs.readFile('lte_networks.html', {
+        encoding: 'utf8'
+    }, function(err, data) {
+        if(err) return console.error(err);
+
+        var d = scrapeLteNetworks(data);
+
+        prettyJSON(d);
     });
+}
+
+function scrapeLteNetworks(data) {
+    var ret = {};
+    var $ = cheerio.load(data);
+
+    $('.wikitable tr').each(function(i, tr) {
+        var name, band;
+
+        if(i > 0) $('td', $(tr)).each(function(i, td) {
+            var itext = $('i', $(td)).text();
+            var text;
+
+            if(itext) text = itext;
+            else text = $(td).text();
+
+            if(i == 1) name = text;
+            if(i == 3 && text.indexOf('?') == -1) band = text;
+            if(i == 5) {
+                if(text == '2013 (planned)' || text == '2013 (in Trial)') band = null;
+            }
+        });
+
+        if(!(name in ret)) ret[name] = [];
+        if(band && ret[name].indexOf(band) == -1) ret[name].push(band);
+        if(!ret[name].length) delete ret[name];
+    });
+
+    return ret;
 }
 
 function scrapeLte(data) {
@@ -24,7 +62,7 @@ function scrapeLte(data) {
     var $ = cheerio.load(data);
 
     $('.lte-chart tr').each(function(i, tr) {
-        return $('.selfclear em', $(tr)).each(function(i, em) {
+        $('.selfclear em', $(tr)).each(function(i, em) {
             var name = $(em).text().split(' ').slice(1).join('');
             var bands = $('td', $(tr)).map(function(i, td) {
                 if(i == 1) return $('h4', $(td)).map(function(i, h4) {
